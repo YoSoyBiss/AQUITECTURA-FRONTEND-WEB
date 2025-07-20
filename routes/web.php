e18caf760894f0161372ca94d0173cc35335fd4e
ArturoZoberzau <ramirez.garturo222.1922@gmail.com>
2025-07-20 14:12:52 -0600

    rutas completas web

--- routes/web.php
+++ routes/web.php
@@ -6,8 +6,12 @@ use App\Http\Controllers\ProductoController;
 Route::get('/productos', [ProductoController::class, 'index']);
 Route::get('/productos/crear', [ProductoController::class, 'crear']);
 Route::post('/productos', [ProductoController::class, 'guardar']);
+Route::delete('/productos/{id}', [ProductoController::class, 'destroy'])->name('productos.destroy');
 
 
+
+Route::get('/productos/{id}/editar', [ProductoController::class, 'editar']);
+Route::put('/productos/{id}', [ProductoController::class, 'actualizar']);
 Route::get('/', function () {
     return view('welcome');
 });
